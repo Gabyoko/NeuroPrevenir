eedce4602bc5def101a2968399fe1532eb56ce5e
Gabr-beep <g987211767@gmail.com>
2024-09-30 23:15:27 -0500

    commit 11

---
+++ src/layouts/headerLayout.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+</script>
+
+<template>
+
+<div class="header">
+
+  <div class="container">
+
+        
+          <img class="img1" src="../assets/neuro.png" alt="">
+        
+
+         <div class="box2">NeuroPrevenir</div>
+
+  </div>
+
+</div>
+
+</template>
+
+<style scoped>
+.header{
+   
+background: #445ca1;
+
+
+
+}
+.container {
+  display: flex;
+ 
+
+  gap: 10px;
+  align-items: center;
+  margin-left: 30px;
+}
+
+
+
+.img1, .box2 {
+
+    color: #c5ecf7;
+ font-size: 22px;
+ 
+  font-family: "Merriweather", serif;
+  font-weight: 700;
+  width: 90px;
+  
+}
+</style>
---
+++ src/layouts/mainLayout.vue
@@ -0,0 +1,13 @@
+<script setup>
+import headerLayout from './headerLayout.vue';
+
+
+</script>
+
+<template>
+<headerLayout></headerLayout>
+</template>
+
+<style scoped>
+
+</style>
